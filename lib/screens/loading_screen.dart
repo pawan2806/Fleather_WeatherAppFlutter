3946d83a5e91e1d996c457f327bc8825df72e3a3
Pawan Singh <pawansingh102938@gmail.com>
2020-09-26 03:26:30 +0530

    First commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    getLocationData();
   }
-}
+
+  void getLocationData() async {
+
+    WeatherModel weatherModel=WeatherModel();
+    var weatherData=await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData,);
+    }));
+  }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(
+          child: SpinKitChasingDots(
+            color: Colors.white,
+            size: 100.0,
+          ),
+        ),
+      );
+    }
+  }
+
+
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,33 @@
+
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter/cupertino.dart';
+
+
+const apiKey='095415277e40ec709087b65aacb4b585';
+const openWeatherMapURL= 'https://api.openweathermap.org/data/2.5/weather';
+
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url='$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper=NetworkHelper(url);
+    var weatherData= await networkHelper.getData();
+    return weatherData;
+  }
+  Future<dynamic> getLocationWeather() async {
+    double latitude;
+    double longitude;
+    Location location = Location();
+    await location.getCurrentLocation();
+    longitude = location.longitude;
+    latitude = location.latitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
